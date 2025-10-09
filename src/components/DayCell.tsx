70a5129d9c90b4806d269365c4aebff3bd32e76f
Elkanah Donkor <elkanahdonkor@gmail.com>
2025-10-09 20:16:48 +0000

    addressed ismobile hook

--- src/components/DayCell.tsx
+++ src/components/DayCell.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { Popover, PopoverTrigger, PopoverContent } from '@/components/ui/popover';
+import { useIsMobile } from '@/hooks/use-mobile';
 import { DailySummary } from './CalendarView'; // Assuming DailySummary is exported from CalendarView
 
 interface DayCellProps {
