e5a5e858a3f475c7f7ffd2c1e9a20d4e0a9b3b6b
Elkanah Donkor <elkanahdonkor@gmail.com>
2025-10-13 23:30:34 +0000

    Added home to  the betafedback page

--- src/app/(beta)/beta/page.tsx
+++ src/app/(beta)/beta/page.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+import Link from 'next/link';
 import { useState } from 'react';
 import AboutBeta from '@/components/beta/AboutBeta';
 import Feedback from '@/components/beta/Feedback';
@@ -10,6 +11,13 @@ const BetaPage = () => {
 
   return (
     <div className="container mx-auto p-4">
+        <div className="mr-2 md:m-0 flex items-center">
+        <Link href="/">
+          <h1 className="text-2xl font-bold text-foreground">
+            Forex<span className="text-emerald-400">Pencil</span>
+          </h1>
+        </Link>
+      </div>
       <div className="flex border-b">
         <button
           className={`py-2 px-4 ${activeTab === 'about' ? 'border-b-2 border-primary' : ''}`}
